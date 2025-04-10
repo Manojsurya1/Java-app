a31e4059f98c829dff551c23d9aed15a4f7a98ed
Manoj Chelkapally <119150620+Manojsurya1@users.noreply.github.com>
2025-04-10 13:56:41 -0700

    Update MinikubeSampleApplicationTests.java

--- src/test/java/com/minikube/sample/MinikubeSampleApplicationTests.java
+++ src/test/java/com/minikube/sample/MinikubeSampleApplicationTests.java
@@ -2,15 +2,25 @@ package com.minikube.sample;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.kubernetes.client.KubernetesClientAutoConfiguration;
+import org.springframework.cloud.kubernetes.discovery.KubernetesDiscoveryClientAutoConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.cloud.kubernetes.enabled=false",
+    "spring.main.allow-bean-definition-overriding=true"
+})
+@ImportAutoConfiguration(exclude = {
+    KubernetesClientAutoConfiguration.class,
+    KubernetesDiscoveryClientAutoConfiguration.class
+})
 public class MinikubeSampleApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
